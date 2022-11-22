8407d97d1c29688bab3a3994702a925a04436fd3
Jorge <jlramirez1210@gmail.com>
2022-11-21 23:16:50 -0500

    clase principal app

--- src/demoInsCurso/dao/InscripcionDao.java
+++ src/demoInsCurso/dao/InscripcionDao.java
@@ -10,7 +10,7 @@ import demoInsCurso.dto.InscripcionDto;
 public class InscripcionDao {
 	public int insertar(InscripcionDto dto) {
 		// el query para obtener la secuencia
-		String s1 = "SELECT MAX(id_inscripcion)+1 FROM inscripcion";
+		String s1 = "SELECT IF(id_insc IS NULL, 1, MAX(id_insc)+1) AS id FROM inscripcion";
 		String s2 = "INSERT INTO inscripcion(id_insc, nombre, telefono, id_curso, if_forma_pago) "
 				+ "VALUES(?,?,?,?,?)";
 
@@ -21,12 +21,12 @@ public class InscripcionDao {
 			ResultSet rs = ps1.executeQuery();
 
 			if (rs.next()) {
-				int max = rs.getRow();
-				ps2.setInt(0, max);
-				ps2.setString(1, dto.getNombre());
-				ps2.setString(2, dto.getTelefono());
-				ps2.setInt(3, dto.getIdCurso());
-				ps2.setInt(4, dto.getIdFormaPago());
+				int max = rs.getInt(1);
+				ps2.setInt(1, max);
+				ps2.setString(2, dto.getNombre());
+				ps2.setString(3, dto.getTelefono());
+				ps2.setInt(4, dto.getIdCurso());
+				ps2.setInt(5, dto.getIdFormaPago());
 
 				if (ps2.executeUpdate() != 1) {
 					throw new RuntimeException("Error ...");
